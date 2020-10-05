821454c82aa0927006a13927986d7e81c7fdf771
Stuart Wallace <stuart@wapbot.co.uk>
2020-10-05 20:38:38 +0100

    Add public key

--- scripts/import_key.sh
+++ scripts/import_key.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 function error {
   echo ${1:-"Unknown error"}
   exit 1
@@ -24,6 +27,11 @@ REPO_OWNER=$(echo $REPO_SLUG | sed 's/\([^\/]*\)\/.*/\1/g')
 if [[ ! -e $KEYHOME/$REPO_OWNER.key.gpg ]]; then
   error "No key for repo owner $REPO_OWNER exists"
 fi
+
+# Import Public Keys
+for f in $KEYHOME/public/*; do
+  { gpg --home $KEYRING_HOME --import $f ; } || { error "Could not import key into gpg." ; }
+done
 
 { echo -n $PASSPHRASE | gpg --decrypt --batch --passphrase-fd 0 --output $KEYHOME/$REPO_OWNER.key $KEYHOME/$REPO_OWNER.key.gpg ; } || error "Failed to decrypt key"
 
