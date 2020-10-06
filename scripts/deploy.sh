c07c0a730dfddd52c044fe9ff32d947a3675e5b7
Stuart Wallace <stuart@wapbot.co.uk>
2020-10-06 10:10:14 +0100

    Add GPG fingerprint debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,8 @@ KEYHOME=$(git rev-parse --show-toplevel)/.keys
 export KEYRING_HOME=$KEYHOME/.gpg
 export GPG_FINGERPRINT=$(gpg --home $KEYRING_HOME --list-keys | grep "^ " | cut -b 7-)
 
+gpg --home $KEYRING_HOME --list-keys
+
+echo "Fingerprint: $GPG_FINGERPRINT"
+
 curl -sL https://git.io/goreleaser | bash
